d805d40dc3407d20f16295f48c7a13cf13583cf7
RizkiMufrizal <mufrizalrizki@gmail.com>
2023-01-04 22:35:01 +0700

    delete mapping imperative

--- Mapping-Imperative-Baseline/src/main/java/org/rizki/mufrizal/baseline/domain/HttpMethod.java
+++
@@ -1,5 +0,0 @@
-package org.rizki.mufrizal.baseline.domain;
-
-public enum HttpMethod {
-    GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH
-}
--- Mapping-Imperative-Baseline/src/main/java/org/rizki/mufrizal/baseline/service/EndPointService.java
+++
@@ -1,9 +0,0 @@
-package org.rizki.mufrizal.baseline.service;
-
-import org.rizki.mufrizal.baseline.domain.EndPoint;
-
-public interface EndPointService {
-    EndPoint findByBackendAndBackendFunction(String backend, String backendFunction);
-
-    void reload();
-}
